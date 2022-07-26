dd84d70167eedae9723ce7789cabf272a5a24d8b
Brigand Shi <brigandshi@BriganddeMacBook-Pro.local>
2022-07-26 10:35:42 +0800

    first commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,103 @@
+export default {
+  themeConfig: {
+    siteTitle: "RTCStack",
+    nav: [
+      {
+        text: '云计算',
+        items: [
+          {
+            items: [
+              { text: '计算榜', link: '/ecs/ecs.md' },
+            ]
+          }
+        ]
+      },
+      {
+        text: '云原生',
+        items: [
+          {
+            items: [
+              { text: '原生榜', link: '/cce/cce.md' },
+            ]
+          }
+        ]
+      },
+      {
+        text: 'SDN',
+        items: [
+          {
+            items: [
+              { text: 'SDN榜', link: '/sdn/sdn.md' },
+            ]
+          }
+        ]
+      },
+      {
+        text: '数据库',
+        items: [
+          {
+            items: [
+              { text: '数据库榜', link: '/rds/rds.md' },
+            ]
+          }
+        ]
+      },
+      {
+        text: '云原生',
+        items: [
+          {
+            items: [
+              { text: '原生榜', link: '/cce/cce.md' },
+            ]
+          }
+        ]
+      },
+      {
+        text: '前端开发',
+        items: [
+          {
+            items: [
+              { text: '前端榜', link: '/fe/fe.md' },
+            ]
+          }
+        ]
+      },
+      {
+        text: '后端开发',
+        items: [
+          {
+            items: [
+              { text: '后端榜', link: '/be/be.md' },
+            ]
+          }
+        ]
+      },
+      {
+        text: '移动开发',
+        items: [
+          {
+            items: [
+              { text: '移动榜', link: '/me/me.md' },
+            ]
+          }
+        ]
+      },
+    ],
+    sidebar: {
+      '/index': [
+        {
+          text: '在线工具',
+          collapsible: true,
+          items: [{ text: 'MD5计算', link: '/tool/md5.html' }]
+        }
+      ],
+      // '/ecs/': [
+      //   {
+      //     text: '云计算',
+      //     collapsible: true,
+      //     items: [{ text: '云计算', link: '/ecs/ecs.md' }]
+      //   }
+      // ]
+    }
+  }
+}
